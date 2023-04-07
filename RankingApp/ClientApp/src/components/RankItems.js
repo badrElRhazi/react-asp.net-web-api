a8fa17ef35e5217b17cea28dc4c9ed2601f0697c
Badr El Rhazi <rzi.badr@gmail.com>
2023-04-07 17:40:15 +0000

    first commit

---
+++ RankingApp/ClientApp/src/components/RankItems.js
@@ -0,0 +1,31 @@
+﻿import React, { useState, useEffect } from 'react';
+import MovieImageArr from './MovieImage.js';
+
+const RankItems = () => {
+
+    const [items, setItems] = useState([]);
+    const dataType = 1;
+
+    useEffect(() => {
+        fetch(`item/${dataType}`)
+            .then((results) => {
+                return results.json();
+            })
+            .then(data => {
+                setItems(data);
+            })
+    },[])
+
+    return (
+        <main>
+            <div className="items-not-ranked"> {
+                (items.length > 0) ? items.map((item) =>
+                    <img id={`item-${item.id}`} src={MovieImageArr.find(o=>o.id===item.imageId)?.image }></img>
+                    ) : <div>Loading...</div>
+                    
+            }
+           </div>
+        </main>
+    )
+}
+export default RankItems;
